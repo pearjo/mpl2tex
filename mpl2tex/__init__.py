cc3cc19b908f2f7beaf29c5631be8bd158173e4b
Joe Pearson <joe.pearson@dlr.de>
2019-02-23 17:21:35 +0100

    Update RcParams class

--- mpl2tex/__init__.py
+++ mpl2tex/__init__.py
@@ -7,5 +7,6 @@ include it as pgf into a LaTeX document.
 """
 __version__ = '1.0.0'
 
-from mpl2tex.rcparams import RcParams
+# from mpl2tex.rcparams import RcParams
 from mpl2tex.rcparams import rcParamsDefault
+from mpl2tex.rcparams import update_params
--- mpl2tex/rcparams.py
+++ mpl2tex/rcparams.py
@@ -1,95 +1,80 @@
 # -*- coding: utf-8 -*-
+"""Update Matplotlib for better LaTeX export."""
 import matplotlib as mpl
 
 
-class RcParams(object):
-    r"""Generate rcParams for matplotlib.
+class RcParams():
+    r"""Generate rcParams for Matplotlib.
 
     Updates the matplotlibrc for best export to LaTeX.
-
-    Parameters
-    ----------
-    width : float
-        Width in relation to columnwidth e.g. ``0.5*columnwidth``.
-        Default is 1.0.
-    columnwidth : float
-        Column width of the LaTeX document. Get the value by using
-        ``\showthe\columnwidth`` in LaTeX.
-    fontsize : float
-        Font size of the LaTeX document. Default is 10.
-    ratio : {float, 'gr', 'sq'}
-        Aspect ratio of the figure. Optional values are 'gr' for golden ratio
-        and 'sq' for square ratio. Default is 'gr'.
-    serif : boolean, optional
-        Set all fonts serif. Default is True.
-    linewidth : float
-        Axes line width in pt. Default is 0.6.
-    frame : float
-        Line width of the figure frame in pt. Default is 0.4.
-    bw : boolean
-        Lines will be black and the linestyle cycles through '-', '--', '-.'
-        and ':'. Default is False.
-    marker : boolean
-        The marker style cycles through all possible matplotlib markers.
-        Default is False.
-
     """
 
-    def __init__(self, width=1.0, columnwidth=345, fontsize=10, linewidth=0.4,
-                 markeredgewidth=0.0, ratio='gr', serif=True, bw=False,
-                 marker=False):
-        self.width = width
-        self.columnwidth = columnwidth
-        self.fontsize = float(fontsize)
-        self.ratio = ratio
-        self.serif = serif
-        self.linewidth = linewidth
-        self.markeredgewidth = markeredgewidth
-        self.everytick = 0.2
-        self.everyaxisgrid = 0.2
-        self.everyaxis = 0.4
-        self.bw = bw
-        self.marker = marker
-
+    def __init__(self):
         self.rcParams = dict()
-        self.__figsize__()
-        self.__params__()
-        mpl.rcParams.update(self.rcParams)
 
-    def __params__(self):
-        """Update rcParams.
-
-        Update the rcParams with a dictionary ``items``.
-
-        """
         items = {'text.usetex': True,
                  'text.latex.unicode': True,
                  'text.latex.preamble': r'\usepackage{amsmath}',
                  'grid.linestyle': '-',
                  'grid.alpha': '0.25',
                  'grid.color': '000000',
-                 'grid.linewidth': self.everyaxisgrid,
-                 'axes.linewidth': self.everyaxis,
-                 'patch.linewidth': self.everyaxis,
-                 'font.size': self.fontsize,
-                 'axes.titlesize': self.fontsize,
-                 'axes.labelsize': self.fontsize,
-                 'xtick.labelsize': self.fontsize,
-                 'ytick.labelsize': self.fontsize,
+                 'grid.linewidth': 0.2,
+                 'axes.linewidth': 0.4,
+                 'patch.linewidth': 0.4,
                  'ytick.direction': 'in',
                  'xtick.direction': 'in',
-                 'xtick.major.width': self.everytick,
-                 'xtick.minor.width': self.everytick,
-                 'ytick.major.width': self.everytick,
-                 'ytick.minor.width': self.everytick,
-                 'figure.figsize': self.figsize,
+                 'xtick.major.width': 0.2,
+                 'xtick.minor.width': 0.2,
+                 'ytick.major.width': 0.2,
+                 'ytick.minor.width': 0.2,
                  'figure.autolayout': True,
-                 'lines.markeredgewidth': self.markeredgewidth,
-                 'lines.linewidth': self.linewidth,
                  'legend.edgecolor': '0.0',
                  'legend.fancybox': False,
                  'legend.frameon': True,
                  'legend.framealpha': 1.0}
+
+        self.rcParams.update(items)
+
+    def update(self, width=1.0, columnwidth=345, fontsize=10, linewidth=0.4,
+               markeredgewidth=0.0, ratio='gr', serif=True, bw=False,
+               marker=False):
+        """Update rcParams.
+
+        Update the rcParams with a dictionary ``items``.
+
+        Parameters
+        ----------
+        width : float
+            Width in relation to columnwidth e.g. ``0.5*columnwidth``.
+            Default is 1.0.
+        columnwidth : float
+            Column width of the LaTeX document. Get the value by using
+            ``\showthe\columnwidth`` in LaTeX.
+        fontsize : float
+            Font size of the LaTeX document. Default is 10.
+        ratio : {float, 'gr', 'sq'}
+            Aspect ratio of the figure. Optional values are 'gr' for golden ratio
+            and 'sq' for square ratio. Default is 'gr'.
+        serif : boolean, optional
+            Set all fonts serif. Default is True.
+        linewidth : float
+            Axes line width in pt. Default is 0.6.
+        frame : float
+            Line width of the figure frame in pt. Default is 0.4.
+        bw : boolean
+            Lines will be black and the linestyle cycles through '-', '--', '-.'
+            and ':'. Default is False.
+        marker : boolean
+            The marker style cycles through all possible matplotlib markers.
+            Default is False.
+
+        """
+        mpl.rcParams.update(mpl.rcParamsDefault)
+        self.__figsize__(width, linewidth, ratio)
+        items = {'font.size': fontsize,
+                 'lines.markeredgewidth': markeredgewidth,
+                 'lines.linewidth': linewidth}
+        self.rcParams.update(items)
 
         if self.serif is True:
             self.rcParams.update({'font.family': 'serif'})
@@ -100,9 +85,9 @@ class RcParams(object):
         if self.marker is True:
             self.__marker__()
 
-        self.rcParams.update(items)
+        mpl.rcParams.update(self.rcParams)
 
-    def __figsize__(self):
+    def __figsize__(self, width, columnwidth, ratio):
         r"""Calculate figure size.
 
         Calculate the figure size for a given ratio, columnwidth
@@ -110,20 +95,18 @@ class RcParams(object):
         command ``\showthe\columnwidth``.
 
         """
-        figwidth_pt = self.columnwidth*self.width
+        figwidth_pt = columnwidth*width
         inches_per_pt = 1.0/72.27  # Convert pt to inches
 
-        if self.ratio == 'gr':
-            ratio = 1.61803  # Aesthetic ratio (1+sqrt(5)) / 2
-        elif self.ratio == 'sq':
+        if ratio == 'gr':
+            ratio = 1.61803  # Aesthetic ratio (1 + sqrt(5))/2
+        elif ratio == 'sq':
             ratio = 1.0
-        else:
-            ratio = self.ratio
 
         figwidth = figwidth_pt * inches_per_pt
         figheight = figwidth/ratio
-
-        self.figsize = [figwidth, figheight]
+        figsize = {'figure.figsize': [figwidth, figheight]}
+        self.rcParams.update(figsize)
 
     def __bw__(self):
         """Black and white.
@@ -151,3 +134,18 @@ class RcParams(object):
 def rcParamsDefault():
     """Reset rcParams to default."""
     mpl.rcParams.update(mpl.rcParamsDefault)
+
+
+def copy_doc(doc):
+    def func_wrapper(func):
+        func.__doc__ = doc
+        return func
+    return func_wrapper
+
+
+@copy_doc(RcParams.update.__doc__)
+def params(width=1.0, columnwidth=345, fontsize=10, linewidth=0.4,
+           markeredgewidth=0.0, ratio='gr', serif=True, bw=False, marker=False):
+    rcParams = RcParams()
+    rcParams.update(width, columnwidth, fontsize, linewidth,
+                    markeredgewidth, ratio, serif, bw, marker)
